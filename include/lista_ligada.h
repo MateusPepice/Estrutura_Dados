e5589be1c929b5e716869df7e30f91ce8c8c5895
Mateus Pepice <112258589+MateusPepice@users.noreply.github.com>
2023-04-19 00:21:09 +0000

    Arquivos alterados

---
+++ include/lista_ligada.h
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+#include<stdlib.h>
+
+typedef struct no No;
+
+void adicionar_final();
+void adicionar_inicio();
+
+void remover_final();
+void remover_inicio();
+
+void buscar_valor();
+void buscar_posicao();
+
+void destruir_lista();
---
+++ src/lista_ligada.c
@@ -0,0 +1,40 @@
+#include "lista_ligada.h"
+#include<stdlib.h>
+#include<stdlib.h>
+
+struct no {
+    int dados;
+    No *ptr_proximo;
+}
+
+No *criar_lista(){
+    
+}
+
+void adicionar_final(No *ptr_auxiliar){
+
+}
+
+void adicionar_inicio(){
+
+}
+
+void remover_final(){
+
+}
+
+void remover_inicio(){
+
+}
+
+void buscar_valor(){
+
+}
+
+void buscar_posicao(){
+
+}
+
+void destruir_lista(){
+
+}
